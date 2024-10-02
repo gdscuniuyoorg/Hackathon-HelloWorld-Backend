8b09a94a475ae4651e22a06c84f1cd35188110ac
princewill <princewillahumaraeze@gmail.com>
2024-10-02 09:58:46 +0100

    Added user model and user registratio api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_distance
+from .views import get_distance, register_user
 
 
 urlpatterns = [
     path('get_distance/', get_distance, name='get_distance'),
+    path('register/', register_user, name='register'),
 ]
